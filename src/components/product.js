a738aec9f2b54a56311f501d90e8d95ebd979af3
aruna9603 <arunaanbazhagan03@gmail.com>
2026-02-09 00:50:42 +0900

    Fix redundant alt attribute warning

--- src/components/product.js
+++ src/components/product.js
@@ -4,17 +4,17 @@ function Product(){
     return(
         <div className="product">
         <div>
-            <img src="https://picsum.photos/200/?perfume?1" alt="photo"/>
+            <img src="https://picsum.photos/200/?perfume?1" alt="perfume"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum in itaque deleniti iure voluptatum sint
                 voluptas ipsam vel aut.</p>
         </div>
         <div>
-            <img src="https://picsum.photos/200/?perfume?2" alt="photo"/>
+            <img src="https://picsum.photos/200/?perfume?2" alt="perfume"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum in itaque deleniti iure voluptatum sint
                 voluptas ipsam vel aut.</p>
         </div>
         <div>
-            <img src="https://picsum.photos/200/?perfume?3" alt="photo"/>
+            <img src="https://picsum.photos/200/?perfume?3" alt="perfume"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero eum in itaque deleniti iure voluptatum sint
                 voluptas ipsam vel aut.</p>
         </div>
